878c36be81081721e367af849e053b2fbf0adf7f
shahnewazIbrahim <shahnewaz886@gmail.com>
2025-05-22 12:26:52 +0600

    question

--- lib/ebook_contents.dart
+++ lib/ebook_contents.dart
@@ -1,9 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:ebook_project/api/api_service.dart';
 import 'package:ebook_project/components/app_layout.dart';
 import 'package:ebook_project/models/ebook_content.dart';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter_html/flutter_html.dart';
 
 class EbookContentsPage extends StatefulWidget {
   final String ebookId;
@@ -12,22 +11,25 @@ class EbookContentsPage extends StatefulWidget {
   final String topicId;
   final String ebookName;
 
-  const EbookContentsPage(
-      {super.key,
-      required this.ebookId,
-      required this.subjectId,
-      required this.chapterId,
-      required this.topicId,
-      required this.ebookName});
+  const EbookContentsPage({
+    super.key,
+    required this.ebookId,
+    required this.subjectId,
+    required this.chapterId,
+    required this.topicId,
+    required this.ebookName,
+  });
 
   @override
-  _EbookContentsState createState() => _EbookContentsState();
+  State<EbookContentsPage> createState() => _EbookContentsPageState();
 }
 
-class _EbookContentsState extends State<EbookContentsPage> {
-  List<EbookContent> ebookContents = []; // Ensure this is initialized as a List
+class _EbookContentsPageState extends State<EbookContentsPage> {
+  List<EbookContent> ebookContents = [];
   bool isLoading = true;
   bool isError = false;
+  Map<int, String> selectedAnswers = {};
+  Set<int> showCorrect = {};
 
   @override
   void initState() {
@@ -39,97 +41,134 @@ class _EbookContentsState extends State<EbookContentsPage> {
     ApiService apiService = ApiService();
     try {
       final data = await apiService.fetchEbookData(
-          "/v1/ebooks/${widget.ebookId}/subjects/${widget.subjectId}/chapters/${widget.chapterId}/topics/${widget.topicId}/contents");
+        "/v1/ebooks/${widget.ebookId}/subjects/${widget.subjectId}/chapters/${widget.chapterId}/topics/${widget.topicId}/contents",
+      );
       setState(() {
         ebookContents = (data['contents'] as List)
-            .map((questionJson) => EbookContent.fromJson(questionJson))
+            .map((e) => EbookContent.fromJson(e))
             .toList();
         isLoading = false;
       });
-    } catch (error) {
+    } catch (e) {
       setState(() {
         isLoading = false;
-        isError = true; // Optional: handle error state
+        isError = true;
       });
-      print("Error fetching ebook topics: $error");
     }
+  }
+
+  Widget buildOptionButtons(EbookContent content) {
+    return Column(
+      children: content.options.asMap().entries.map((entry) {
+        final option = entry.value;
+        final index = entry.key;
+        String answerKey = content.answer[index];
+        String? selected = selectedAnswers[option.id];
+        bool correctShown = showCorrect.contains(content.id);
+
+        return Row(
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: correctShown
+                    ? (answerKey == 'T'
+                    ? Colors.green
+                    : Colors.red)
+                    : (selected == 'T' ? Colors.blue : Colors.grey),
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedAnswers[option.id] = 'T';
+                });
+              },
+              child: const Text('T'),
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: correctShown
+                    ? (answerKey == 'F'
+                    ? Colors.green
+                    : Colors.red)
+                    : (selected == 'F' ? Colors.blue : Colors.grey),
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedAnswers[option.id] = 'F';
+                });
+              },
+              child: const Text('F'),
+            ),
+            const SizedBox(width: 8),
+            Expanded(child: Html(data: option.title)),
+          ],
+        );
+      }).toList(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return AppLayout(
-      // title: 'Ebook Topics',
       title: '${widget.ebookName} Questions',
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: ebookContents.length,
-                padding: const EdgeInsets.all(8.0),
-                itemBuilder: (context, index) {
-                  if (ebookContents[index].title.isNotEmpty) {
-                    return GestureDetector(
-                      onTap: () {
-                        // Add navigation logic to the 'Chapters' screen with parameters
-                      },
-                      child: Card(
-                        color: Colors.grey[200],
-                        margin: EdgeInsets.symmetric(vertical: 8.0),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            children: [
-                              ShaderMask(
-                                shaderCallback: (bounds) {
-                                  return LinearGradient(
-                                    colors: [
-                                      Colors.blue[200]!,
-                                      Colors.blue[600]!
-                                    ],
-                                    begin: Alignment.topLeft,
-                                    end: Alignment.bottomRight,
-                                  ).createShader(bounds);
-                                },
-                                child: Icon(
-                                  FontAwesomeIcons.book,
-                                  color: Colors.white,
-                                  size: 30,
-                                ),
-                              ),
-                              SizedBox(width: 10.0),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.7,
-                                    // child: Text(
-                                    //   ebookContents[index].title,
-                                    //   style: TextStyle(
-                                    //     fontSize: 16.0,
-                                    //     fontWeight: FontWeight.bold,
-                                    //   ),
-                                    //   overflow: TextOverflow.ellipsis,
-                                    //   maxLines: 2,
-                                    // ),
-                                    child: Html(
-                                        data:
-                                        ebookContents[index].title,
-                                  ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: ebookContents.length,
+        itemBuilder: (context, index) {
+          final content = ebookContents[index];
+          return Card(
+            margin: const EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Html(data: content.title),
+                  const SizedBox(height: 8),
+                  buildOptionButtons(content),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            showCorrect.add(content.id);
+                          });
+                        },
+                        child: const Text("Answer"),
                       ),
-                    );
-                  } else {
-                    return const SizedBox.shrink(); // Skip empty titles
-                  }
-                },
+                      const SizedBox(width: 8),
+                      if (content.hasDiscussion)
+                        ElevatedButton(
+                          onPressed: () {
+                            // TODO: Discussion Modal
+                          },
+                          child: const Text("Discussion"),
+                        ),
+                      const SizedBox(width: 8),
+                      if (content.hasReference)
+                        ElevatedButton(
+                          onPressed: () {
+                            // TODO: Reference Modal
+                          },
+                          child: const Text("Reference"),
+                        ),
+                      const SizedBox(width: 8),
+                      if (content.hasSolveVideo)
+                        ElevatedButton(
+                          onPressed: () {
+                            // TODO: Solve Video Modal
+                          },
+                          child: const Text("Video"),
+                        ),
+                    ],
+                  )
+                ],
               ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/models/ebook_content.dart
+++ lib/models/ebook_content.dart
@@ -1,23 +1,62 @@
+class Option {
+  final int id;
+  final String slNo;
+  final String title;
+
+  Option({
+    required this.id,
+    required this.slNo,
+    required this.title,
+  });
+
+  factory Option.fromJson(Map<String, dynamic> json) {
+    return Option(
+      id: json['id'],
+      slNo: json['sl_no'],
+      title: json['title'],
+    );
+  }
+}
+
 class EbookContent {
   final int id;
   final String title;
+  final int type;
+  final int pageNo;
+  final List<Option> options;
+  final String answer;
+  final bool hasDiscussion;
+  final bool hasReference;
+  final bool hasSolveVideo;
+  final bool hasNote;
 
   EbookContent({
     required this.id,
     required this.title,
+    required this.type,
+    required this.pageNo,
+    required this.options,
+    required this.answer,
+    required this.hasDiscussion,
+    required this.hasReference,
+    required this.hasSolveVideo,
+    required this.hasNote,
   });
 
   factory EbookContent.fromJson(Map<String, dynamic> json) {
     return EbookContent(
       id: json['id'],
       title: json['title'],
+      type: json['type'],
+      pageNo: json['page_no'],
+      options: (json['options'] as List)
+          .map((option) => Option.fromJson(option))
+          .toList(),
+      answer: json['answer'],
+      hasDiscussion: json['has_discussion'],
+      hasReference: json['has_reference'],
+      hasSolveVideo: json['has_solve_video'],
+      hasNote: json['has_note'],
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-    };
   }
 }
